867f637b7cdfff60bb698049d4d5a690af7b6f76
sultan <sultan-malik@hotmail.co.uk>
2018-10-05 22:33:40 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-	var nav = document.getElementById('nav');
-	var spana = document.getElementsByTagName('span');
+	var nav = document.querySelector('.navbar');
+	var span = document.getElementsByTagName('span');
 	var brand = document.getElementById('brand');
-	var a = document.getElementById('links').getElementsByTagName('a');
+	var links = document.getElementById('links').getElementsByTagName('a');
 	const menuOne = document.querySelector('.menuOne');
 	var toggleStatus = 1;
 
@@ -10,7 +10,8 @@
 window.onscroll = function() {
 
 		if (window.pageYOffset > 150) {
-			nav.style.background = "white";
+			nav.style.background = "rgb(0,0,0, 0.7)";
+			nav.style.height = "60px";
 			
 
 			// brand.firstElementChild.style.color = "#000";
@@ -29,6 +30,7 @@ window.onscroll = function() {
 
 	 	else {
 	 		nav.style.background = "transparent";
+	 		
 			// brand.firstElementChild.style.color = "#f8f8f8";
 
 			// for (i = 0; i < span.length; i++) {
@@ -42,9 +44,21 @@ window.onscroll = function() {
 		}
 	}
 
+
+$(".carousel-control").focus(function(event){
+    $(this).blur();
+});
+
 	function toggleMenu() {
 		if (toggleStatus == 1) {
-			document.getElementById('menu').style.width = '400px';
+			document.getElementById('menu').style.width = '100%';
+			$('body').addClass('stop-scrolling');
+
+			for (j = 0; j < links.length; j++) {
+				  links[j].style.fontSize = '0';
+				}
+			
+
 			toggleStatus = 0;
 			menuOne.classList.toggle("clickMenuOne");
 
@@ -52,6 +66,12 @@ window.onscroll = function() {
 			document.getElementById('menu').style.width = '0';
 			toggleStatus = 1;
 			menuOne.classList.remove("clickMenuOne");
+			$('body').removeClass('stop-scrolling')
+
+			for (j = 0; j < links.length; j++) {
+				  links[j].style.fontSize = '16px';
+				}
+
 		}
 	}
 
@@ -66,3 +86,36 @@ window.onscroll = function() {
 			easing: 'swing'
 		});
 	});
+
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+       		labels: ["HTML5", "CSS3", "JavaScript", "jQuery" , "PHP" , "Version Control (git)", "Bootstrap", "Java", "C++", "MySQL"],
+       		datasets: [{
+            label: '# of Votes',
+            data: [50, 50, 50, 10, 15, 20, 15, 20, 10, 15],
+            backgroundColor: [
+                'rgba(255, 99, 132, .5)',
+                'rgba(54, 162, 235, .5)',
+                'rgba(255, 206, 86, .5)',
+                'rgba(25, 206, 86, .5)',
+                'rgba(255, 26, 86, .5)',
+                'rgba(55, 206, 86, .5)',
+                'rgba(255, 206, 86, .5)',
+                'rgba(25, 206, 8, .5)',
+                'rgba(55, 06, 86, .5)',
+                'rgba(55, 76, 86, .5)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+});
